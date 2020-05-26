63545a8d036f2dc67d60711318ab62adde0c4a15
Valentin <valentin.genev@modusbox.com>
2020-05-26 07:48:31 +0300

    fixed unit tests

--- test/unit/Tracer.test.ts
+++ test/unit/Tracer.test.ts
@@ -311,12 +311,12 @@ describe('Tracer', () => {
 
       let IVChild = Tracer.createChildSpanFromContext('service4', { ...extractedContext })
       IVChild.setTracestateTags({ bar: 'baz' })
-      expect(IVChild.getContext().tracestates[mockConfig.EVENT_LOGGER_VENDOR_PREFIX]).toEqual({ bar: 'baz', spanId: IIChild.getContext().spanId })
-      expect(IVChild.getTracestateTags()).toEqual({ bar: 'baz', spanId: IIChild.getContext().spanId })
-      expect(IVChild.getTracestates()[mockConfig.EVENT_LOGGER_VENDOR_PREFIX]).toEqual({ bar: 'baz', spanId: IIChild.getContext().spanId })
-      // expect(IIChild.spanContext.spanId).toBe(IVChild.spanContext.parentSpanId)
-      // expect(tracer.spanContext.traceId).toBe(IVChild.spanContext.traceId)
-      // expect(IIIChild.spanContext.service).toBe('service4')
+      expect(IVChild.getContext().tracestates[mockConfig.EVENT_LOGGER_VENDOR_PREFIX]).toEqual({ bar: 'baz', spanId: IVChild.getContext().spanId })
+      expect(IVChild.getTracestateTags()).toEqual({ bar: 'baz', spanId: IVChild.getContext().spanId })
+      expect(IVChild.getTracestates()[mockConfig.EVENT_LOGGER_VENDOR_PREFIX]).toEqual({ bar: 'baz', spanId: IVChild.getContext().spanId })
+      expect(IIChild.spanContext.spanId).toBe(IVChild.spanContext.parentSpanId)
+      expect(tracer.spanContext.traceId).toBe(IVChild.spanContext.traceId)
+      expect(IIIChild.spanContext.service).toBe('service4')
 
       let newMessageC = IIIChild.injectContextToMessage({ trace: {} })
       let expected1 = { trace: IIIChild.getContext() }  
